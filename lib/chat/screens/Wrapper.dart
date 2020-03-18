527575c09b9582c0cdce998ae1663a010844e0f4
AnujPandey007 <panuj330@gmail.com>
2020-03-18 19:16:23 +0530

    A new

--- lib/chat/screens/Wrapper.dart
+++ lib/chat/screens/Wrapper.dart
@@ -3,6 +3,20 @@ import 'package:hello_world/chat/screens/authenticate/Authenticate.dart';
 import 'package:hello_world/chat/screens/home/Home.dart';
 import 'package:provider/provider.dart';
 import 'package:hello_world/chat/models/user.dart';
+import 'package:hello_world/chat/services/auth.dart';
+
+class Myapp3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Wrapper()
+      ),
+    );
+  }
+}
 
 
 class Wrapper extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/chat/services/auth.dart';
 import 'package:hello_world/practice.dart';
-import 'package:provider/provider.dart';
 import './chat/screens/Wrapper.dart';
-import './chat/screens/home/Home.dart';
 import './app_screen/Login.dart';
 import './Calculator/Calculator.dart';
-import 'package:provider/provider.dart';
-import './chat/models/user.dart';
 
 void main() => runApp(Myapp3());
 
-class Myapp3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthService().user,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Wrapper()
-      ),
-    );
-  }
-}
+
 
