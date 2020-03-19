cee15a1678de88ca6422efe95f195aa8da42adda
AnujPandey007 <panuj330@gmail.com>
2020-03-19 21:26:04 +0530

    5

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,28 +1,33 @@
 void main() {
-
-  //Example 1: Passing Function to higher order function
-  Function addNumbers = (int a,int b) => print(a+b);
-  someOtherFunction("anuj", addNumbers);
-
-  //Example 2: Receiving Function from higher order function
-  var myFun = taskToPerform();
-  print(myFun(4));
-
-}
-
-//Higher Order Function:--
-
-
-//Example one: Accepts Function as parameters
-void someOtherFunction(String message, Function myFunction) {         //Higher Order Function
+  String message="Dart";
   print(message);
-  print(myFunction(2,4));
 
+  Function showMessage = () {
+    message = "Dart is awesome";
+    print(message);
+  };
+
+  showMessage();
+
+  Function talk = () {
+    String msg = "Hi";
+    print(msg);
+
+    Function say = (){
+      msg = "hello";
+      print(msg);
+    };
+
+    return say;
+
+  };
+
+  
+
+  var speak = talk();
+  speak();
+  
 }
 
-//Example two: Returns a Function
-Function taskToPerform() {                                              //Higher Order Function
 
-  Function multiplyFour = (int number) => number*4;
-  return multiplyFour;
-}
+
