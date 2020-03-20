2bdd43699aa77635e1027eb453900c715684cf61
AnujPandey007 <panuj330@gmail.com>
2020-03-20 18:53:31 +0530

    Map

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,39 +1,43 @@
-void main(){
+void main() {
 
-// 1st way 
+  //1st way
 
-Set<String> country = Set.from(["INDIA", "USA","CHINA"]);
-country.add("JAPAN");
+  Map<String, int> countriesCode = {
+    "INDIA":91,
+    "USA": 1,
+    "PAKISTAN":92
+  };
 
-country.forEach((String element) {
-  print(element);
-});
+  //2nd way
 
-//2nd way
+  Map<String, String> fruits = Map(); //Here String(1st) is Key and String(2nd) is value  -- Map<k,v>
 
-Set<int> numberSet = Set();  // List is of integer and its growable list
-numberSet.add(73);                  
-numberSet.add(64);
-numberSet.add(21);                    // List is nothing but array
-numberSet.add(12);
+  fruits["apple"]="red";              //Here apple is Key and red is value
+  fruits["banana"]="yellow";
+  fruits["guava"]="green";     
 
-print(numberSet.length);
+  print(fruits["apple"]);
 
-print("\n");
+  print("\n");
 
-for(int element in numberSet){         //Printing value from for-each loop
+  for(String key in fruits.keys){         //Printing keys
+    print(key);
+  }
+  print("\n");
+  for(String value in fruits.values){     //Printing Values
+    print(value);
+  }
 
-  print(element);
+  print("\n");
+
+  fruits.forEach((key, value) => print(fruits));  
+
+  print("\n");
+
+  fruits.update("apple", (value) => "Blue");  // Updating the data
+
+  fruits.forEach((key, value) => print("key is $key and value is $value"));
+
+  print("\n");
+
 }
-
-print("\n");
-
-numberSet.forEach((int elements)=>print(elements));   //Using Lambda function
-
-
-print("\n");
-
-
-}
-
-
