6ac5902701a15fb6929a5cfef005fe65ee6c4d5b
AnujPandey007 <panuj330@gmail.com>
2020-03-22 16:54:21 +0530

    Search Bar

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/pages/choose_location.dart';
-import 'package:hello_world/pages/home_screen.dart';
-import 'package:hello_world/pages/loading.dart';
 
 
 class App extends StatelessWidget {
@@ -10,12 +7,62 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: "World Map",
       debugShowCheckedModeBanner: false,
-      initialRoute: "/",
-      routes: {
-        "/": (context) => Load(),
-        "/home":(context) => HomeScreen(),
-        "/location":(context) => ChooseLocation()
-      },
+      home: HomeP(),
+    );
+  }
+}
+
+class HomeP extends StatefulWidget {
+  @override
+  _HomePState createState() => _HomePState();
+}
+
+class _HomePState extends State<HomeP> {
+  
+  Icon custombutton = Icon(Icons.search);
+  Widget customSearchBar = Text("Search Bar");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: customSearchBar,
+        actions: <Widget>[
+          IconButton(
+            icon: custombutton, 
+            onPressed: (){
+              setState(() {
+                if(this.custombutton.icon == Icons.search){
+                  this.custombutton = Icon(Icons.cancel);
+                  this.customSearchBar = TextField(
+                    textInputAction: TextInputAction.go,
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search Here",
+                      hintStyle: TextStyle(
+                        color: Colors.white70
+                      )
+                    ),
+                  );
+                }
+                else{
+                  this.custombutton = Icon(Icons.search);
+                  this.customSearchBar = Text("Search Bar");
+                }
+              });
+            },
+          ),
+        ],
+
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Search"),
+        ),
+      ),
     );
   }
 }
