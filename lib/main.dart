a4b6e4285cb1d47d9a3f9a5fdad6329e91c56924
AnujPandey007 <panuj330@gmail.com>
2020-03-25 11:43:42 +0530

    Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/note/practice.dart';
-import './chat/screens/Wrapper.dart';
-import './app_screen/Login.dart';
-import './Calculator/Calculator.dart';
-import './pro.dart';
+import 'package:hello_world/note/h.dart';
+import './practice.dart';
 
 void main() => runApp(Myapp4());
 
